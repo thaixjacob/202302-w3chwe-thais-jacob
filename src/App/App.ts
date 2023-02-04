5c0a91fa4496449c3bcf73b4da3d5eb908b43b23
`thais` <`tjlannes@gmail.com`>
2023-02-04 18:03:45 +0100

    add img pokemon

--- src/App/App.ts
+++ src/App/App.ts
@@ -5,7 +5,7 @@ export default class App extends Component {
   #header: Header;
   constructor(parentElement: HTMLElement | undefined) {
     super(parentElement, "app-container", "div");
-    this.#header = new Header(this.domElement, "hola");
+    this.#header = new Header(this.domElement, "./assets/pokemon-logo.svg");
   }
 
   render(): void {
